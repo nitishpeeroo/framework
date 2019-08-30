36254c9b3e9649063a57b688e7d06e54458821e4
NITISH PEEROO <nitish.peeroo@gmail.com>
2019-08-30 02:01:10 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    use Framework\App;
+    use GuzzleHttp\Psr7\ServerRequest;
+
+    require '../vendor/autoload.php';
+
+    $app = new App();
+
+    $response = $app->run(ServerRequest::fromGlobals());
+    \Http\Response\send($response);
